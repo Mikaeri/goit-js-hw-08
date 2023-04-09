fa49873603ff196a22efdb5c3869ce4d3fab36fa
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-09 21:43:46 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const STORAGE_PLAYER_KEY = 'videoplayer-current-time';
+const LOCAL_STRAGE_PLAYER_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
 
-function onPlayerTimeupdate(data) {
-  const currenTime = data.seconds;
+function onPlayerTimeupdate(timer) {
+  const currentTime = timer.seconds;
 
-  localStorage.setItem(STORAGE_PLAYER_KEY, currenTime);
+  localStorage.setItem(LOCAL_STRAGE_PLAYER_KEY, currentTime);
 }
 
-player.setCurrentTime(localStorage.getItem(STORAGE_PLAYER_KEY) || 0);
+player.setCurrentTime(localStorage.getItem(LOCAL_STRAGE_PLAYER_KEY) || 0);
