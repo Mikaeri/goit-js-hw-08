3211cc2d2718d7597aae6642b95af2284ecf4a4e
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-09 21:40:27 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,33 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+
+// const iframe = document.querySelector('');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('timeupdate',throttle(onPlayerTimeUpdate, 1000));
+
+// function onPlayerTimeUpdate(time){
+//     const currentTime = time.seconds;
+
+//     localStorage.setItem("videoplayer-current-time", currentTime);
+// }
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_PLAYER_KEY = 'videoplayer-current-time';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
 
-player.on('timeupdate', onPlayerTimeUpdate, 1000);
+function onPlayerTimeupdate(data) {
+  const currenTime = data.seconds;
 
-function onPlayerTimeUpdate(time){
-    const currentTime = time.seconds;
-
-    localStorage.setItem('videoplayer-current-time', currentTime);
+  localStorage.setItem(STORAGE_PLAYER_KEY, currenTime);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+player.setCurrentTime(localStorage.getItem(STORAGE_PLAYER_KEY) || 0);
