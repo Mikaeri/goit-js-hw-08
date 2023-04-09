156b850bad9213e72b3bcf1fd4f22529cd8e04cb
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-09 21:28:53 +0100

    task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', onPlayerTimeUpdate, 1000);
+
+function onPlayerTimeUpdate(time){
+    const currentTime = time.seconds;
+
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
