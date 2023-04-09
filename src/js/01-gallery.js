a90d914c42e3e5cb1b3dd900428d6aa9fade1929
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-09 20:56:58 +0100

    task-1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector(".js-container");
+const markup = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`);
+
+gallery.insertAdjacentHTML('beforeend', markup.join(""));
+let lightBox = new SimpleLightbox('.gallery a',{    
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+
+
+
 
 console.log(galleryItems);
